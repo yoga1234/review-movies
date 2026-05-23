24098887ca861061b34b1df814585fbb61f1144a
yoga1234 <septianayoga30@gmail.com>
2026-05-23 20:50:38 +0700

    adding code for footer and header

---
+++ README.md
@@ -0,0 +1,6 @@
+<!-- color pallete yang digunakan -->
+
+#222831
+#393E46
+#00ADB5
+#EEEEEE
---
+++ assets/module/module.js
@@ -0,0 +1,22 @@
+const header = document.getElementById("header");
+const footer = document.getElementById("footer");
+
+header.innerHTML = `
+  <!-- header start -->
+  <div class="navbar">
+    <div class="left-side">MOVIE REVIEW</div>
+    <div class="right-side">
+      <a href="#">HOME</a>
+      <a href="./pages/movielist.html">MOVIE LIST</a>
+      <a href="./pages/daftarlogin.html">LOGIN</a>
+    </div>
+  </div>
+`;
+
+footer.innerHTML = `
+  <div class="top">
+    <div class="left-side">© Movie Review Limited. 2026 Copyrighted</div>
+    <div class="right-side">MOVIE REVIEW</div>
+  </div>
+  <div class="bottom">-Created with PRIDE by YOGASMARA and ZUFAR-</div>
+`;
