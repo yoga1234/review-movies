a5d20d208a9c73d08926173e0f1452fcbfad2619
abdzufar <zufarzufar@gmail.com>
2026-05-27 23:27:54 +0700

    add object reviewData & CRUD review

--- assets/js/movieDetails.js
+++ assets/js/movieDetails.js
@@ -1,1 +1,24 @@
-console.log(location.pathname);
+function createUpdateReview () {
+    inputReviewDOM = document.getElementById("inputReview");
+    inputRatingDOM = document.getElementById("inputRating");
+    if (inputReviewDOM && inputRatingDOM) {
+        activeMovieId = localStorage.getItem("activeMovie");
+        currentUserUsername = JSON.parse(localStorage.getItem("currentUser")).username;
+        reviewData[activeMovieId][currentUserUsername].review = inputReviewDOM.value; // reviewData declared in reviewData.js
+        reviewData[activeMovieId][currentUserUsername].rating= inputRatingDOM.value;
+        localStorage.setItem("reviewData", JSON.stringify(reviewData));
+    }
+}
+
+function deleteReview() {
+    activeMovieId = localStorage.getItem("activeMovie");
+    currentUserUsername = JSON.parse(localStorage.getItem("currentUser")).username;
+    reviewData[activeMovieId][currentUserUsername] = undefined;
+    localStorage.setItem("reviewData", JSON.stringify(reviewData));
+}
+
+// activeMovieId = localStorage.getItem("activeMovie");
+// currentUserUsername = JSON.parse(localStorage.getItem("currentUser")).username;
+// console.log(activeMovieId, currentUserUsername);
+// console.log(reviewData);
+// console.log(localStorage.getItem("activeMovie"), typeof(localStorage.getItem("activeMovie")));
