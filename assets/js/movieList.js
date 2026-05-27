465808eda15f86d0e1ece00d3b408aa2d7bebf9d
yoga1234 <septianayoga30@gmail.com>
2026-05-27 16:55:05 +0700

    adding button behaviour for set activeMovie on localStorage

--- assets/js/movieList.js
+++ assets/js/movieList.js
@@ -15,6 +15,10 @@ function cutSynopsis(synopsisString) {
   return result;
 }
 
+function setActiveMovie(id) {
+  localStorage.setItem("activeMovie", String(id));
+}
+
 function renderMovieList(data) {
   movieListCardContainer.innerHTML = "";
 
@@ -32,7 +36,7 @@ function renderMovieList(data) {
             <p class="card-text">
             ${cutSynopsis(data[i].synopsis)}
             </p>
-            <a href="#" class="btn btn-primary">Lihat Detail</a>
+            <a onclick="setActiveMovie(${data[i].id})"  href="./movieDetails.html" class="btn btn-primary">Lihat Detail</a>
         </div>
       </div>
     `;
